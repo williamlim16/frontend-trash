6d1dd11812fb7f472ee2b0ab7c81e5eff4245f02
william <williaml1601@gmail.com>
2022-03-17 13:36:02 +0700

    crud trash can done

--- src/components/TrashCan.js
+++ src/components/TrashCan.js
@@ -2,7 +2,7 @@ import DataTable from 'react-data-table-component';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
-import { EditIcon, AddIcon } from '@chakra-ui/icons';
+import { EditIcon, AddIcon, DeleteIcon } from '@chakra-ui/icons';
 import {
   Button,
   Flex,
@@ -19,86 +19,165 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Text,
 } from '@chakra-ui/react';
-
-const columns = [
-  {
-    name: 'ID',
-    selector: row => row.ID,
-    maxWidth: '20px',
-  },
-  {
-    name: 'Name',
-    selector: row => row.Name,
-  },
-  {
-    name: 'Location',
-    selector: row => row.Location,
-  },
-  {
-    name: 'User',
-    selector: row => row.User.Email,
-  },
-  {
-    name: 'Action',
-    button: true,
-    cell: row => {
-      return (
-        <a href="/">
-          <EditIcon />
-        </a>
-      );
-    },
-  },
-];
 
 function TrashCan() {
   //Init data
   let [data, setData] = useState([]);
-  let [values, setValues] = useState({});
+  let [values, setValues] = useState({
+    mode: '',
+    id: '',
+    name: '',
+    location: '',
+  });
 
+  //Modal control
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  //Get state from redux
   let token = useSelector(state => state.user.value.token);
   let email = useSelector(state => state.user.value.email);
   let userID = useSelector(state => state.user.value.ID);
-  useEffect(() => {
-    //Get token for jwt
-    let config = {
-      headers: {
-        Authorization: 'Bearer ' + token,
-      },
-    };
 
+  //JWT stufss
+  let config = {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  };
+
+  /**
+   * Initialize data table
+   */
+  useEffect(() => {
     //Fetch data from API
     axios.get('/api/trashcans', config).then(res => {
       setData(res.data.data);
     });
   }, []);
-  //API call to register new trash can
-  const requestRegisterTrashCan = e => {
-    e.preventDefault();
-    console.log(values);
 
-    axios.post('/api/trashcan', {
-      name: values.name,
-      location: values.location,
-      userid: JSON.stringify(userID),
-      headers: {
-        Authorization: 'Bearer ' + token,
-      },
+  //Helper function for refreshing data
+  const getData = () => {
+    axios.get('/api/trashcans', config).then(res => {
+      setData(res.data.data);
     });
+  };
+
+  //API call to register & edit data
+  const submitForm = e => {
+    e.preventDefault();
+
+    if (values.mode == 'add') {
+      axios
+        .post('/api/trashcan', {
+          name: values.name,
+          location: values.location,
+          userid: JSON.stringify(userID),
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        })
+        .then(() => {
+          onClose();
+          getData();
+        });
+    } else {
+      axios
+        .put('/api/trashcan/' + values.id, {
+          name: values.name,
+          location: values.location,
+          userid: JSON.stringify(userID),
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        })
+        .then(() => {
+          onClose();
+          getData();
+        });
+    }
   };
 
   //update values according to inputs
   const formChangeHandler = name => e => {
     setValues({ ...values, [name]: e.target.value });
   };
+
+  /**
+   * Modifying modal content
+   */
+
+  //For editing entry
+  const editTrashCan = row => {
+    onOpen();
+    setValues({
+      ...values,
+      name: row.Name,
+      location: row.Location,
+      id: row.ID,
+      mode: 'edit',
+    });
+  };
+
+  //For adding entry
+  const addTrashCan = () => {
+    onOpen();
+    setValues({ ...values, name: '', location: '', mode: 'add' });
+  };
+
+  /**
+   * Delete entry
+   */
+  const deleteTrashCan = row => {
+    axios.delete('/api/trashcan/' + row.ID).then(() => {
+      getData();
+    });
+  };
+
+  /**
+   * RDT configs
+   */
+  const columns = [
+    {
+      name: 'ID',
+      selector: row => row.ID,
+      maxWidth: '20px',
+    },
+    {
+      name: 'Name',
+      selector: row => row.Name,
+    },
+    {
+      name: 'Location',
+      selector: row => row.Location,
+    },
+    {
+      name: 'User',
+      selector: row => row.User.Email,
+    },
+    {
+      name: 'Action',
+      button: true,
+      cell: row => {
+        return (
+          <>
+            <Button onClick={() => editTrashCan(row)}>
+              <EditIcon />
+            </Button>
+            <Button onClick={() => deleteTrashCan(row)}>
+              <DeleteIcon />
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
   return (
     <>
       <Flex mb="1rem">
         <Spacer />
-        <Button onClick={onOpen}>
+        <Button onClick={() => addTrashCan()}>
           <AddIcon />
         </Button>
       </Flex>
@@ -107,17 +186,22 @@ function TrashCan() {
       <Modal isOpen={isOpen} onClose={onClose} size="lg">
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create Trash Can</ModalHeader>
+          {values.mode == 'add' ? (
+            <ModalHeader>Create Trash Can</ModalHeader>
+          ) : (
+            <ModalHeader>Edit Trash Can</ModalHeader>
+          )}
+
           <ModalCloseButton />
           <ModalBody>
-            <form onSubmit={requestRegisterTrashCan}>
+            <form onSubmit={submitForm}>
               <VStack>
                 <FormControl>
                   <FormLabel htmlFor="name">Name</FormLabel>
                   <Input
                     id="name"
                     type="text"
-                    values={values.name}
+                    value={values.name}
                     onChange={formChangeHandler('name')}
                   />
                 </FormControl>
@@ -126,7 +210,7 @@ function TrashCan() {
                   <Input
                     id="location"
                     type="text"
-                    values={values.location}
+                    value={values.location}
                     onChange={formChangeHandler('location')}
                   />
                 </FormControl>
--- src/components/layout/RegisterLoginLay.js
+++ src/components/layout/RegisterLoginLay.js
@@ -1,10 +1,12 @@
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, Box } from '@chakra-ui/react';
 
 const RegisterLoginLay = ({ children }) => {
   return (
     <Grid templateColumns={'repeat(7,1fr)'} gap={5}>
       <GridItem colStart={3} colEnd={6}>
-        {children}
+        <Box boxShadow="md" p="2rem" rounded="xl">
+          {children}
+        </Box>
       </GridItem>
     </Grid>
   );
